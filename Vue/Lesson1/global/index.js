291f5c52fea9e2413a8f52e6b6f5a6e85fdc6159
karta2341940 <a08487a@gmail.com>
2022-11-25 15:24:08 +0800

    update

--- Vue/Lesson1/global/index.js
+++ Vue/Lesson1/global/index.js
@@ -1,22 +1,15 @@
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import component1 from "bundle-text:./component/test.html"
 const Example = {
-    template:component1
-  };
-  
-  const app = {
-    data(){
-        return{
-            data:"Hello This is data"
-        }
-    },
-    components: {
-      Example
-    },
-    template: `
-      <h3>Vue 3</h3>
-      {{data}}
-      <Example />
-    `
-  };
+  template: component1
+};
+
+const app = {
+  data() {
+    return {
+      data: "Hello This is data"
+    }
+  }
+}
+
 createApp(app).mount("#app")
